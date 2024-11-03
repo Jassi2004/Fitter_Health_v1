5815622e473d2d10a0272668f28ce6b2e57b9cbb
Jaskirat Singh <bedijaskirat2004@gmail.com>
2024-11-03 21:43:09 +0530

    started working on dashboard

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { verifyEmail, signup, login, logout, forgotPassword, resetPassword } = require('../controllers/authController');
 
-router.post('/signup',signup);
-router.post('/verifyOtp',verifyEmail);
+router.post('/signup', signup);
+router.post('/verifyOtp', verifyEmail);
 router.post('/login', login);
-router.post('/logout',logout);
+router.post('/logout', logout);
 router.post('/forgotPassword', forgotPassword);
 router.post('/resetPassword', resetPassword);
 
